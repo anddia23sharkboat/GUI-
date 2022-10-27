8c8a64f5b7482f88116528a11749ca65f6999993
Andrey Diaz-Ortega <>
2022-10-27 17:48:30 +0000

    Initial commit

---
+++ Flag.java
@@ -0,0 +1,39 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Flag {
+
+    public static void main(String[] args) {
+    
+	  JPanel greenPanel = new JPanel();
+      
+		greenPanel.setBackground(Color.green);
+		greenPanel.setBounds(0, -100, 150, 600);
+		greenPanel.setLayout(new BorderLayout());
+		
+		JPanel whitePanel = new JPanel();
+		whitePanel.setBackground(Color.white);
+		whitePanel.setBounds(150, -100, 150, 600);
+		whitePanel.setLayout(new BorderLayout());
+		
+		JPanel redPanel = new JPanel();
+		redPanel.setBackground(Color.red);
+		redPanel.setBounds(300, -100, 180, 600);
+		redPanel.setLayout(new BorderLayout());
+		
+		JFrame frame = new JFrame();
+    frame.setTitle("Italy Flag");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(null);
+		frame.setSize(750,750);
+		frame.setVisible(true);	
+		frame.add(greenPanel);
+		frame.add(whitePanel);
+		frame.add(redPanel);
+    frame.setVisible(true);
+     
+    }
+}
