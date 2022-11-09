e1d033099d98d59ca30fe6a17675a1762ebc1fc3
Andrey Diaz-Ortega <116588449+anddia23sharkboat@users.noreply.github.com>
2022-11-09 02:19:30 +0000

    I did my project for GUI-4

---
+++ GUI/PaintApp/ColorButton.java
@@ -0,0 +1,23 @@
+package GUI.PaintApp;
+
+import java.awt.*;
+import java.util.Random;
+import javax.swing.*;
+
+public class ColorButton extends JButton {
+//class variables
+public static Random rand = new Random();
+//instance variables   
+private String name;
+private Color color;
+  
+  public ColorButton() {
+    this.name= "Try me";
+    this.color= new Color(rand.nextInt(255),rand.nextInt(255),rand.nextInt(255));
+    this.setBackground(this.color);
+    this.setText(this.name);
+ }
+public Color getColor(){
+     return this.color;
+ }
+}
--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
 import Game.Clicker1;
+import GUI.PaintApp.ColorText; 
 
 public class Main {
     public static void main(String[] args){
-        new Clicker1();
+        //new Clicker1();
+        new ColorText(); 
     }
 }
